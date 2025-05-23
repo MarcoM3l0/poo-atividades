7bf55003f7cef954677fc2805584bc7d7c903d95
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-05-23 18:00:33 -0300

    fix: adição da sobrepõe equals() e hashCode() na classe Usuario

--- OO_Atividade_06/src/br/ifs/grasp/model/Usuario.java
+++ OO_Atividade_06/src/br/ifs/grasp/model/Usuario.java
@@ -1,5 +1,7 @@
 package br.ifs.grasp.model;
 
+import java.util.Objects;
+
 public class Usuario {
 	
 	private String nome;
@@ -12,5 +14,25 @@ public class Usuario {
 		this.email = email;
 		this.telefone = telefone;
 	}
+
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return Objects.hashCode(email);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		Usuario usuario = (Usuario) obj;
+		return Objects.equals(this.email, usuario.email);
+	}
+	
+	
 
 }
