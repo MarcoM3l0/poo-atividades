36c77ed992d2fb52e4d680f7a1aa3f3aaaeccdcf
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-25 16:46:13 -0300

    feat(flyweight): adiciona classe Bloco com estado extrínseco e referência para TipoBloco

--- Padrões de Projeto/estrutural/br/edu/ifs/designpatterns/flyweight/impl/Bloco.java
+++ Padrões de Projeto/estrutural/br/edu/ifs/designpatterns/flyweight/impl/Bloco.java
@@ -1,5 +1,32 @@
 package br.edu.ifs.designpatterns.flyweight.impl;
 
 public class Bloco {
+	
+	private int x;
+	private int y;
+	private int z;
+	private TipoBloco tipo;
+	
+	public Bloco(int x, int y, int z, TipoBloco tipo) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.tipo = tipo;
+	}
+	
+	public String mostrar() {
+		return this.toString();
+	}
+
+	public TipoBloco getTipo() {
+		return tipo;
+	}
+
+	@Override
+	public String toString() {
+		return tipo.mostrar() + ", x: " + x + ", y: " + y + ", z: " + z;
+	}
+	
+	
 
 }
