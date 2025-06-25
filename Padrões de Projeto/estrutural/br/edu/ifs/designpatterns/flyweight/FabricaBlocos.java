686f1e0a0bd4ff02414fe68a10c061ccf5805866
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-25 16:52:47 -0300

    feat(flyweight): adiciona FabricaBlocos para gerenciamento de instâncias TipoBloco

--- Padrões de Projeto/estrutural/br/edu/ifs/designpatterns/flyweight/FabricaBlocos.java
+++ Padrões de Projeto/estrutural/br/edu/ifs/designpatterns/flyweight/FabricaBlocos.java
@@ -1,5 +1,21 @@
 package br.edu.ifs.designpatterns.flyweight;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import br.edu.ifs.designpatterns.flyweight.impl.TipoBloco;
+
 public class FabricaBlocos {
+	
+	private static  Map<String, TipoBloco> mapaBlocos = new HashMap<String, TipoBloco>();
+
+	public static TipoBloco obterTipo(String nome, boolean quebravel, boolean inflamavel, boolean empilhavel) {
+		
+		if(!mapaBlocos.containsKey(nome)) {
+			mapaBlocos.put(nome, new TipoBloco(nome, quebravel, inflamavel, empilhavel));
+		}
+		
+		return mapaBlocos.get(nome);
+	}
 
 }
