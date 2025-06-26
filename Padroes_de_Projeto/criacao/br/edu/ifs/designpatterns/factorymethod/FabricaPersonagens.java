2f750f42393c1404853ec774ad5cd9275907a692
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 16:47:14 -0300

    feat(personagens): implementar FabricaPersonagens Creator

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/FabricaPersonagens.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/FabricaPersonagens.java
@@ -1,5 +1,10 @@
 package br.edu.ifs.designpatterns.factorymethod;
 
+import br.edu.ifs.designpatterns.factorymethod.impl.Anao;
+import br.edu.ifs.designpatterns.factorymethod.impl.Elfo;
+import br.edu.ifs.designpatterns.factorymethod.impl.Humano;
+import br.edu.ifs.designpatterns.factorymethod.impl.Orc;
+
 public class FabricaPersonagens {
 
 	public static Personagem criarPersonagem(String personagem) {
