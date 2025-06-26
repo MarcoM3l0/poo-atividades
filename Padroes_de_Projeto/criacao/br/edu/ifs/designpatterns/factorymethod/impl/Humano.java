61971496e98778bd8cec65e2ca2ebce794fcbb73
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 17:03:07 -0300

    feat(product/humano): criar Concrete Product Humano implementando Personagem

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/impl/Humano.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/impl/Humano.java
@@ -1,5 +1,25 @@
 package br.edu.ifs.designpatterns.factorymethod.impl;
 
-public class Humano {
+import br.edu.ifs.designpatterns.factorymethod.Personagem;
+
+public class Humano implements Personagem{
+
+	@Override
+	public String atacar() {
+		// TODO Auto-generated method stub
+		return "Ataque: espada";
+	}
+
+	@Override
+	public String defender() {
+		// TODO Auto-generated method stub
+		return "Defesa: rolar";
+	}
+
+	@Override
+	public String usarMagia() {
+		// TODO Auto-generated method stub
+		return "Magia: raio de luz";
+	}
 
 }
