c2dbebc8387a84f2b77d9106720f6ed111aa1bb8
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 17:46:47 -0300

    feat(factory/abstract): criar interface FabricaInterfaceGrafica

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/abstractfactory/FabricaInterfaceGrafica.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/abstractfactory/FabricaInterfaceGrafica.java
@@ -1,5 +1,9 @@
 package br.edu.ifs.designpatterns.abstractfactory;
 
 public interface FabricaInterfaceGrafica {
+	
+	public Janela criarJanela();
+	public Botao criarBotao();
+	public Menu criarMenu();
 
 }
