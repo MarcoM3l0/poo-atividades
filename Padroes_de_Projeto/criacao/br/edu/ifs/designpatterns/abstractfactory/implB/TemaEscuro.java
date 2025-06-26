dd649cee0d635c44943caa4146b201bbec4ece67
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 18:13:00 -0300

    feat(factory/ui/dark-theme): adicionar TemaEscuro Concrete Factory

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/abstractfactory/implB/TemaEscuro.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/abstractfactory/implB/TemaEscuro.java
@@ -1,5 +1,28 @@
 package br.edu.ifs.designpatterns.abstractfactory.implB;
 
-public class TemaEscuro {
+import br.edu.ifs.designpatterns.abstractfactory.Botao;
+import br.edu.ifs.designpatterns.abstractfactory.FabricaInterfaceGrafica;
+import br.edu.ifs.designpatterns.abstractfactory.Janela;
+import br.edu.ifs.designpatterns.abstractfactory.Menu;
+
+public class TemaEscuro implements FabricaInterfaceGrafica{
+
+	@Override
+	public Janela criarJanela() {
+
+		return new JanelaEscura();
+	}
+
+	@Override
+	public Botao criarBotao() {
+
+		return new BotaoEscuro();
+	}
+
+	@Override
+	public Menu criarMenu() {
+		
+		return new MenuEscuro();
+	}
 
 }
