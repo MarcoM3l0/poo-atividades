9aa6fb8b74cd1abb3a7eeb510798ed9ae1cbd94b
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 19:03:31 -0300

    feat(builder): adicionar classe Loja como diretor da construção de computadores

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/builder/Loja.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/builder/Loja.java
@@ -1,5 +1,25 @@
 package br.edu.ifs.designpatterns.builder;
 
+import br.edu.ifs.designpatterns.builder.impl.Computador;
+
 public class Loja {
+
+	private Construtor construtor;
+	
+	public Loja(Construtor construtor) {
+		this.construtor = construtor;
+	}
+
+	public Computador construirNotebookGamer() {
+		return new Computador("Core i7-13650HX", 16, 1024);
+	}
+	
+	public Computador construirNotebook() {
+		return new Computador("Core™ i7-1355U", 16, 512);
+	}
+	
+	public Computador construirNotebookUltra() {
+		return new Computador("Core Ultra 7 155H", 32, 1024);
+	}
 
 }
