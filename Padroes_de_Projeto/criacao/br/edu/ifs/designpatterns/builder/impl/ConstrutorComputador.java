71461f4d5513ed687ec3c929a4aa827bce97fc5d
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 18:47:04 -0300

    feat(builder): implementar ConstrutorComputador como builder concreto

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/builder/impl/ConstrutorComputador.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/builder/impl/ConstrutorComputador.java
@@ -1,5 +1,35 @@
 package br.edu.ifs.designpatterns.builder.impl;
 
-public class ConstrutorComputador {
+import br.edu.ifs.designpatterns.builder.Construtor;
+
+public class ConstrutorComputador implements Construtor{
+	
+	private String processador;
+	private int ram;
+	private int hd;
+	
+	@Override
+	public Computador obterResultado() {
+		// TODO Auto-generated method stub
+		return new Computador(this.processador, this.ram, this.hd);
+	}
+	
+	@Override
+	public void definirProcessador(String processador) {
+		// TODO Auto-generated method stub
+		this.processador = processador;
+	}
+	
+	@Override
+	public void definirRAM(int memoriaRam) {
+		// TODO Auto-generated method stub
+		this.ram = memoriaRam;
+	}
+	
+	@Override
+	public void definirArmazenamento(int memoriaRom) {
+		// TODO Auto-generated method stub
+		this.hd = memoriaRom;
+	}
 
 }
