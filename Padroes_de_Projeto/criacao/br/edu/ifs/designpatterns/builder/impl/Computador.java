e2264148a8b0c0bbe5aeaf231d12936ac966baea
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 18:45:52 -0300

    feat(builder): adicionar classe Computador com atributos e métodos de acesso

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/builder/impl/Computador.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/builder/impl/Computador.java
@@ -1,5 +1,28 @@
 package br.edu.ifs.designpatterns.builder.impl;
 
 public class Computador {
+	
+	private String processador;
+	private int ram;
+	private int hd;
+	
+	public Computador(String processador, int ram, int hd) {
+		super();
+		this.processador = processador;
+		this.ram = ram;
+		this.hd = hd;
+	}
+
+	public String getProcessador() {
+		return processador;
+	}
+
+	public int getRam() {
+		return ram;
+	}
+
+	public int getHd() {
+		return hd;
+	}
 
 }
