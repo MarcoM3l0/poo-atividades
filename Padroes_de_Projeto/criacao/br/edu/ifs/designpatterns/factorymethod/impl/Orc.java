5f14b2523b3cfbd2aed8ad05db971419749598ce
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 16:50:51 -0300

    feat(personagens): adicionar classe Orc Concrete Product

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/impl/Orc.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/impl/Orc.java
@@ -1,5 +1,25 @@
 package br.edu.ifs.designpatterns.factorymethod.impl;
 
-public class Orc {
+import br.edu.ifs.designpatterns.factorymethod.Personagem;
+
+public class Orc implements Personagem{
+
+	@Override
+	public String atacar() {
+		// TODO Auto-generated method stub
+		return "Ataque: garras";
+	}
+
+	@Override
+	public String defender() {
+		// TODO Auto-generated method stub
+		return "Defesa: absorver";
+	}
+
+	@Override
+	public String usarMagia() {
+		// TODO Auto-generated method stub
+		return "Magia: espirito maligno";
+	}
 
 }
