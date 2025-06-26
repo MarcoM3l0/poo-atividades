5e7e189a9247b4cc9a67c516c07889d65fb13852
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 16:55:45 -0300

    feat(product/elfo): criar Concrete Product Elfo implementando Personagem

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/impl/Elfo.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/impl/Elfo.java
@@ -1,5 +1,25 @@
 package br.edu.ifs.designpatterns.factorymethod.impl;
 
-public class Elfo {
+import br.edu.ifs.designpatterns.factorymethod.Personagem;
+
+public class Elfo implements Personagem {
+
+	@Override
+	public String atacar() {
+		// TODO Auto-generated method stub
+		return "Ataque: arco e flecha";
+	}
+
+	@Override
+	public String defender() {
+		// TODO Auto-generated method stub
+		return "Defesa: esquiva";
+	}
+
+	@Override
+	public String usarMagia() {
+		// TODO Auto-generated method stub
+		return "Magia: cura";
+	}
 
 }
