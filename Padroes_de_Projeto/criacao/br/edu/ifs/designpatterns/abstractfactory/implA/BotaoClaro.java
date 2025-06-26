e578db3fbe014630973ba9730ff156bb976363ef
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 17:40:24 -0300

    feat(factory/abstract): adicionar interfaces Botao, Janela e Menu Abstract Product

---
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/abstractfactory/implA/BotaoClaro.java
@@ -0,0 +1,5 @@
+package br.edu.ifs.designpatterns.abstractfactory.implA;
+
+public class BotaoClaro {
+
+}
---
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/abstractfactory/implB/MenuEscuro.java
@@ -0,0 +1,5 @@
+package br.edu.ifs.designpatterns.abstractfactory.implB;
+
+public class MenuEscuro {
+
+}
