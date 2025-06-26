fa550b3f23a4dc6d17802a1f76b4f1acc39b3c73
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 16:54:15 -0300

    feat(product/anao): criar Concrete Product Anao implementando Personagem

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/impl/Anao.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/impl/Anao.java
@@ -1,5 +1,25 @@
 package br.edu.ifs.designpatterns.factorymethod.impl;
 
-public class Anao {
+import br.edu.ifs.designpatterns.factorymethod.Personagem;
+
+public class Anao implements Personagem {
+
+	@Override
+	public String atacar() {
+		// TODO Auto-generated method stub
+		return "Ataque: machado";
+	}
+
+	@Override
+	public String defender() {
+		// TODO Auto-generated method stub
+		return "Defesa: escudo";
+	}
+
+	@Override
+	public String usarMagia() {
+		// TODO Auto-generated method stub
+		return "Magia: resistência";
+	}
 
 }
