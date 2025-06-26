3543b3e903657c8a925fb539995cf759510da704
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 19:20:17 -0300

    feat(prototype): implementar RoboTanque como protótipo concreto

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/prototype/impl/RoboTanque.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/prototype/impl/RoboTanque.java
@@ -1,5 +1,70 @@
 package br.edu.ifs.designpatterns.prototype.impl;
 
-public class RoboTanque {
+import java.util.Objects;
 
+import br.edu.ifs.designpatterns.prototype.Robo;
+
+public class RoboTanque implements Robo{
+	
+	private int forca;
+	private int velocidade;
+	private String armamento;
+	
+	public RoboTanque(int forca, int velocidade, String armamento) {
+		this.forca = forca;
+		this.velocidade = velocidade;
+		this.armamento = armamento;
+	}
+
+
+	@Override
+	public Robo clonar() {
+		// TODO Auto-generated method stub
+		return new RoboTanque(this.forca, this.velocidade, this.armamento);
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(forca, velocidade, armamento);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || getClass() != obj.getClass()) return false;
+        RoboTanque that = (RoboTanque) obj;
+        return forca == that.forca && velocidade == that.velocidade && Objects.equals(armamento, that.armamento);
+	}
+
+
+	public int getForca() {
+		return forca;
+	}
+
+
+	public void setForca(int forca) {
+		this.forca = forca;
+	}
+
+
+	public int getVelocidade() {
+		return velocidade;
+	}
+
+
+	public void setVelocidade(int velocidade) {
+		this.velocidade = velocidade;
+	}
+
+
+	public String getArmamento() {
+		return armamento;
+	}
+
+
+	public void setArmamento(String armamento) {
+		this.armamento = armamento;
+	}
+	
 }
