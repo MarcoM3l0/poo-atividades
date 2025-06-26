f8de2707fd3866cd5eec10437123fdb398576a83
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 16:42:24 -0300

    feat(personagens): implementar FabricaPersonagens Creator

--- Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/FabricaPersonagens.java
+++ Padroes_de_Projeto/criacao/br/edu/ifs/designpatterns/factorymethod/FabricaPersonagens.java
@@ -2,4 +2,18 @@ package br.edu.ifs.designpatterns.factorymethod;
 
 public class FabricaPersonagens {
 
+	public static Personagem criarPersonagem(String personagem) {
+		
+		Personagem personagemCriado = switch (personagem) {
+			case "Anao" -> new Anao();
+			case "Elfo" ->  new Elfo();
+			case "Orc" ->  new Orc();
+			case "Humano" ->  new Humano();
+			default -> throw new IllegalArgumentException("Personagem não encontrado!");
+		};
+
+		
+		return personagemCriado;
+	}
+
 }
