81c6b9bc4fb4c75bddb576a27c93a8f89128c90a
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 22:48:47 -0300

    feat(adapter): criar AdaptadorString para integrar RepositorioString à interface Repositorio

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/adapter/impl/AdaptadorString.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/adapter/impl/AdaptadorString.java
@@ -1,5 +1,38 @@
 package br.edu.ifs.designpatterns.adapter.impl;
 
-public class AdaptadorString {
+import java.util.HashMap;
+import java.util.Map;
 
+import br.edu.ifs.designpatterns.adapter.Repositorio;
+import br.edu.ifs.designpatterns.adapter.RepositorioString;
+
+public class AdaptadorString implements Repositorio{
+
+	private RepositorioString repositorio;
+
+	public AdaptadorString(RepositorioString repositorio) {
+		this.repositorio = repositorio;
+	}
+
+	@Override
+	public Map<String, String> obterAlunos() {
+		// TODO Auto-generated method stub
+		
+		Map<String, String> alunos = new HashMap<>();
+		String dados = this.repositorio.recuperarDados();
+		
+		for(String dado : dados.split(";")) {
+			
+			String[] partes = dado.split("\\s", 2);
+			String matricula = partes[0];
+			String nome = partes[1];
+			
+			alunos.put(matricula, nome);
+		}
+		
+		return alunos;
+	}
+	
+	
+	
 }
