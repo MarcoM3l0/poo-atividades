25986bb9fa50a89cea8b84a0a76b54ff1f889920
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 23:09:33 -0300

    feat(bridge): criar interface Plataforma como implementação do padrão Bridge

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/Plataforma.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/Plataforma.java
@@ -1,5 +1,11 @@
 package br.edu.ifs.designpatterns.bridge;
 
+import java.util.List;
+
+import br.edu.ifs.designpatterns.bridge.obj.Ponto;
+
 public interface Plataforma {
+	
+	public List<Ponto> animar(Ponto Ponto1, Ponto Ponto2);
 
 }
