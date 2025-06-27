6be91113f3430bbe90002ba7d90df394c952b9ba
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 22:48:04 -0300

    feat(adapter): implementar AdaptadorLista para adaptar RepositorioLista à interface Repositorio

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/adapter/impl/AdaptadorLista.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/adapter/impl/AdaptadorLista.java
@@ -1,5 +1,37 @@
 package br.edu.ifs.designpatterns.adapter.impl;
 
-public class AdaptadorLista {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.edu.ifs.designpatterns.adapter.Repositorio;
+import br.edu.ifs.designpatterns.adapter.RepositorioLista;
+
+public class AdaptadorLista implements Repositorio{
+	
+	private RepositorioLista repositorio;
+
+	public AdaptadorLista(RepositorioLista repositorioLista) {
+		this.repositorio = repositorioLista;
+	}
+
+	public Map<String, String> obterAlunos() {
+		// TODO Auto-generated method stub
+		List<String> linhas = this.repositorio.recuperarDados();
+		Map<String, String> alunos = new HashMap<>();
+		
+		for(String linha : linhas) {
+			
+			String[] partes = linha.split("\\s", 2);
+			
+			String matricula = partes[0];
+			String nome = partes[1];
+			
+			alunos.put(matricula, nome);
+			
+		}
+		
+		return alunos;
+	}
 
 }
