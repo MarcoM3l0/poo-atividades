a07c7cdc69491440667d922761ad10ef1acfb6a6
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-26 23:31:10 -0300

    feat(bridge): adicionar classe Ponto como estrutura de coordenadas para o padrão Bridge

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/obj/Ponto.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/obj/Ponto.java
@@ -4,5 +4,20 @@ public class Ponto {
 	
 	private double x;
 	private double y;
+	
+	public Ponto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	
 
 }
