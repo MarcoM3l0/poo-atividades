84fb527758e61fddae1459b54da39f0f89bd3e2b
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 20:41:42 -0300

    feat(Concrete Implementation): implementar classes NintendoSwitch, XBoxSeriesX, Playstation5 e PC como concretas de Plataforma

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/plat/Playstation5.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/plat/Playstation5.java
@@ -1,5 +1,38 @@
 package br.edu.ifs.designpatterns.bridge.plat;
 
-public class Playstation5 {
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifs.designpatterns.bridge.Plataforma;
+import br.edu.ifs.designpatterns.bridge.obj.Ponto;
+
+public class Playstation5 implements Plataforma{
+	
+	private int fps = 3;
+	
+	@Override
+	public List<Ponto> animar(Ponto inicio, Ponto fim) {
+		// TODO Auto-generated method stub
+		
+		List<Ponto> trajetoria = new ArrayList<>();
+		
+		double deltaX = fim.getX() - inicio.getX();
+		double deltaY = fim.getY() - inicio.getY();
+		
+		for(int i = 0; i <= getFPS(); i++) {
+			
+			double progresso = (double) i / getFPS();
+			double x = inicio.getX() + (deltaX * progresso);
+			double y = inicio.getY() + (deltaY * progresso);
+			trajetoria.add(new Ponto(x, y));
+		}
+		
+		return trajetoria;
+	}
+
+	
+	public int getFPS() {
+		return fps;
+	}
 
 }
