97964245b533a0f30df580b66de1c56561d15047
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 20:45:17 -0300

    feat(Refined Abstraction): adicionar classe HollowKnight com métodos andar, correr e pular

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/obj/HollowKnight.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/obj/HollowKnight.java
@@ -1,5 +1,63 @@
 package br.edu.ifs.designpatterns.bridge.obj;
 
-public class HollowKnight {
+import java.util.List;
+
+import br.edu.ifs.designpatterns.bridge.ObjetoGrafico;
+import br.edu.ifs.designpatterns.bridge.Plataforma;
+
+public class HollowKnight extends ObjetoGrafico {
+	
+	private double impulso;
+	private double velocidade;
+
+	public HollowKnight(Plataforma plataforma) {
+		super(plataforma);
+		// TODO Auto-generated constructor stub
+		this.impulso = 15;
+		this.velocidade = 15;
+	}
+
+	public List<Ponto> andar(boolean andar) {
+		// TODO Auto-generated method stub
+		
+		 if(andar) {
+			 return super.moverDireita();
+		 }else {
+			 return super.moverEsquerda();
+		 }
+		
+	}
+
+	public List<Ponto> correr(boolean correr) {
+		// TODO Auto-generated method stub
+		double velocidadeOriginal = this.velocidade;
+	    this.velocidade *= 2; 
+	    
+	    try {
+	    	
+	        if(correr) {
+	        	return super.moverDireita();
+	        } else {
+	        	return super.moverEsquerda();
+	        }
+	        
+	    } finally {
+	        this.velocidade = velocidadeOriginal; 
+	    }
+	}
+	
+	public List<Ponto> pular() {
+		// TODO Auto-generated method stub
+		
+		return super.moverPular();
+	}
+	
+	public double getImpulso() {
+		return impulso;
+	}
+
+	public double getVelocidade() {
+		return velocidade;
+	}
 
 }
