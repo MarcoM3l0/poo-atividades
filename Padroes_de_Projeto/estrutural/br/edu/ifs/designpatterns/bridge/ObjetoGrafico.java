10f82c6d0cef063b1a2939c46e858ab12d94d915
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 20:43:01 -0300

    feat(Abstraction): implementar classe abstrata ObjetoGrafico com métodos de movimentação

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/ObjetoGrafico.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/ObjetoGrafico.java
@@ -1,5 +1,65 @@
 package br.edu.ifs.designpatterns.bridge;
 
+import java.util.List;
+
+import br.edu.ifs.designpatterns.bridge.obj.Ponto;
+
 public abstract class ObjetoGrafico {
+	
+	private Ponto posicao;
+	private Plataforma plataforma;
+
+	public abstract double getVelocidade();
+	public abstract double getImpulso();
+	
+	public ObjetoGrafico(Plataforma plataforma) {
+		this.plataforma = plataforma;
+		this.posicao = new Ponto(0, 0);
+	}
+	
+	public List<Ponto> moverDireita(){
+		
+		Ponto inicio = getPosicao();
+		Ponto fim = new Ponto((getPosicao().getX() + getVelocidade()), getPosicao().getY());
+		
+		List<Ponto> trajetoria = getPlataforma().animar(inicio, fim);
+		this.posicao = trajetoria.get(trajetoria.size() - 1);
+		return trajetoria;
+	}
+	
+	public List<Ponto> moverEsquerda(){
+		
+		Ponto inicio = getPosicao();
+		Ponto fim = new Ponto((getPosicao().getX() - getVelocidade()), getPosicao().getY());
+		
+		List<Ponto> trajetoria = getPlataforma().animar(inicio, fim);
+		this.posicao = trajetoria.get(trajetoria.size() - 1);
+		
+		return trajetoria;
+	}
+	
+	public List<Ponto> moverPular(){
+		
+		Ponto inicio = getPosicao();
+		Ponto topo = new Ponto(getPosicao().getX(), (getPosicao().getY() + getImpulso()));
+
+		List<Ponto> subida = getPlataforma().animar(inicio, topo);
+		List<Ponto> decida = getPlataforma().animar(topo, inicio);
+		
+		subida.addAll(decida);
+		
+		return subida;
+		
+	}
+
+	public Ponto getPosicao() {
+		return posicao;
+	}
+
+	public Plataforma getPlataforma() {
+		return plataforma;
+	}
+	
+	
 
 }
