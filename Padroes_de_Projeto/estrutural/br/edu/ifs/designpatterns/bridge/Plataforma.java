5e3db2dd188611b9b3680fdbd917bb546df4bf79
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 20:40:05 -0300

    feat(Implementation): adicionar interface Plataforma com método animar(Ponto, Ponto)

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/Plataforma.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/bridge/Plataforma.java
@@ -6,6 +6,6 @@ import br.edu.ifs.designpatterns.bridge.obj.Ponto;
 
 public interface Plataforma {
 	
-	public List<Ponto> animar(Ponto Ponto1, Ponto Ponto2);
+	public List<Ponto> animar(Ponto inicio, Ponto fim);
 
 }
