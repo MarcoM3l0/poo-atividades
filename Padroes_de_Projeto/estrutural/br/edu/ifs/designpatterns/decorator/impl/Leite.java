fa2a6dbfabf92fd0cc090dd5efc9beb950b77c76
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 22:49:58 -0300

    feat(Concrete Decorator): implementar classe Leite como decorador de Component

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/decorator/impl/Leite.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/decorator/impl/Leite.java
@@ -1,5 +1,24 @@
 package br.edu.ifs.designpatterns.decorator.impl;
 
-public class Leite {
+import br.edu.ifs.designpatterns.decorator.Component;
+
+public class Leite extends Decorator {
+
+	public Leite(Component component) {
+		super(component);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double custo() {
+		// TODO Auto-generated method stub
+		return super.component.custo() + 0.5;
+	}
+
+	@Override
+	public String getDescricao() {
+		// TODO Auto-generated method stub
+		return super.component.getDescricao() + ", com leite";
+	}
 
 }
