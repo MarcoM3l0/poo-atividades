17376c6dd48872461c6fe1c68dc59aca8607cb16
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 22:07:53 -0300

    feat(Composite): implementar classe Combo com agregação de Componentes e cálculo de preço total

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/composite/impl/Combo.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/composite/impl/Combo.java
@@ -1,5 +1,43 @@
 package br.edu.ifs.designpatterns.composite.impl;
 
-public class Combo {
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifs.designpatterns.composite.Componente;
+
+public class Combo extends Componente {
+	
+	private List<Componente> componentes = new ArrayList<>();
+
+	public Combo(String nome) {
+		super(nome);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double calcularPreco() {
+		// TODO Auto-generated method stub
+		
+		double valorTotal = 0;
+		
+		for(Componente componente : componentes) {
+			valorTotal += componente.calcularPreco();
+		}
+		
+		return valorTotal;
+	}
+
+	public void adicionar(Componente item) {
+		// TODO Auto-generated method stub
+		componentes.add(item);
+	}
+	
+	public void remover(Componente item) {
+		componentes.remove(item);
+	}
+	
+	public Componente obter(int index) {
+		return componentes.get(index);
+	}
 
 }
