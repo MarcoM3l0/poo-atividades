f6745a684bcda5a8d125b16f64fdc7b3cee6f40c
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 00:12:53 -0300

    feat(Invoker): adicionar classe ControleRemoto com métodos para configurar e executar comandos

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/ControleRemoto.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/ControleRemoto.java
@@ -1,5 +1,20 @@
 package br.edu.ifs.designpatterns.command;
 
 public class ControleRemoto {
+	
+	protected Comando comando;
+
+	public void pressionarBotao() {
+		// TODO Auto-generated method stub
+		if(this.comando == null) {
+			throw new IllegalStateException("Comando não definido");
+		}
+		
+		this.comando.executar();
+	} 
+	
+	public void definirComando(Comando comando) {
+		this.comando = comando;
+	}
 
 }
