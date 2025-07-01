0d223fc27e7a64a61bb1e6396cd276ec5514a669
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 00:44:35 -0300

    feat(Concrete Command): adicionar comandos para navegar entre canais na Televisao

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/impl/AvancarCanalTV.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/impl/AvancarCanalTV.java
@@ -1,5 +1,21 @@
 package br.edu.ifs.designpatterns.command.impl;
 
-public class AvancarCanalTV {
+import br.edu.ifs.designpatterns.command.Comando;
+import br.edu.ifs.designpatterns.command.Televisao;
+
+public class AvancarCanalTV implements Comando {
+	
+	private Televisao tv;
+
+	public AvancarCanalTV(Televisao tv) {
+		// TODO Auto-generated constructor stub
+		this.tv = tv;
+	}
+
+	@Override
+	public void executar() {
+		// TODO Auto-generated method stub
+		this.tv.setCanal(this.tv.getCanal() + 1);
+	}
 
 }
--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/impl/RetrocederCanalTV.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/impl/RetrocederCanalTV.java
@@ -1,5 +1,21 @@
 package br.edu.ifs.designpatterns.command.impl;
 
-public class RetrocederCanalTV {
+import br.edu.ifs.designpatterns.command.Comando;
+import br.edu.ifs.designpatterns.command.Televisao;
+
+public class RetrocederCanalTV implements Comando {
+	
+	private Televisao tv;
+
+	public RetrocederCanalTV(Televisao tv) {
+		// TODO Auto-generated constructor stub
+		this.tv = tv;
+	}
+
+	@Override
+	public void executar() {
+		// TODO Auto-generated method stub
+		this.tv.setCanal(this.tv.getCanal() - 1);
+	}
 
 }
