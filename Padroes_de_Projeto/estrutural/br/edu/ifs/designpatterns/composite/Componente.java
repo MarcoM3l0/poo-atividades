ef97c466cb8c6407620ebeb6775cb4a7b4b207a7
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 21:51:29 -0300

    feat(Composite): adicionar Componente abstrato com método calcularPreco

---
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/composite/Componente.java
@@ -0,0 +1,13 @@
+package br.edu.ifs.designpatterns.composite;
+
+public abstract class Componente {
+	
+	private String nome;
+
+	public Componente(String nome) {
+		this.nome = nome;
+	}
+	
+	public abstract double calcularPreco();
+
+}
