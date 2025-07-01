705803dd14a912f494ab686ccddebebb298cfbb8
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 23:45:07 -0300

    feat(Concrete Handlers): implementar AnalistaCredito com validação de renda na cadeia de análise

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/chain/impl/AnalistaCredito.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/chain/impl/AnalistaCredito.java
@@ -1,5 +1,18 @@
 package br.edu.ifs.designpatterns.chain.impl;
 
-public class AnalistaCredito {
+import br.edu.ifs.designpatterns.chain.Analisador;
+
+public class AnalistaCredito extends Analisador {
+
+	@Override
+	public Emprestimo processarRequisição(RequisicaoEmprestimo requisicao) {
+		// TODO Auto-generated method stub
+		
+		if(requisicao.getValor() > (0.3 * requisicao.getRenda())) {
+			throw new IllegalStateException("Analista de crédito: Reprovado! Renda insuficiente.");
+		}
+		
+		return sucessor.processarRequisição(requisicao);
+	}
 
 }
