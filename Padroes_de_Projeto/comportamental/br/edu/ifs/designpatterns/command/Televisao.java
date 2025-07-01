0425d11c6a03150d7d8a6a230b36b4276c4309d7
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 00:39:44 -0300

    feat(Receiver): incluir atributo canal e métodos get/set na classe Televisao

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/Televisao.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/Televisao.java
@@ -4,9 +4,11 @@ package br.edu.ifs.designpatterns.command;
 public class Televisao {
 	
 	private boolean ligada;
+	private int canal;
 	
 	public Televisao(String nome) {
 		// TODO Auto-generated constructor stub
+		this.canal = 1;
 	}
 
 	public void ligar() {
@@ -22,5 +24,13 @@ public class Televisao {
 	public boolean estaLigada() {
 		// TODO Auto-generated method stub
 		return this.ligada;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+
+	public void setCanal(int canal) {
+		this.canal = canal;
 	}
 }
