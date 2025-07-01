69aa3a9d8cf38f37e3c2615d50ef67ece2869b69
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 22:57:22 -0300

    feat(Concrete Decorator): implementar classe Acucar como decorador de Component

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/decorator/impl/Acucar.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/decorator/impl/Acucar.java
@@ -1,5 +1,24 @@
 package br.edu.ifs.designpatterns.decorator.impl;
 
-public class Acucar {
+import br.edu.ifs.designpatterns.decorator.Component;
+
+public class Acucar extends Decorator {
+
+	public Acucar(Component component) {
+		super(component);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double custo() {
+		// TODO Auto-generated method stub
+		return super.component.custo() + 0.3;
+	}
+
+	@Override
+	public String getDescricao() {
+		// TODO Auto-generated method stub
+		return super.component.getDescricao() + ", com açúcar";
+	}
 
 }
