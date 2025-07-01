dd20fcbec0f1a42eebec9ebf2bc900e2456f7250
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 20:05:31 -0300

    feat(Concrete Collection): implementar PlaylistMusicas com método criarIterador e gerenciamento de músicas

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/iterator/impl/PlaylistMusicas.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/iterator/impl/PlaylistMusicas.java
@@ -1,5 +1,36 @@
 package br.edu.ifs.designpatterns.iterator.impl;
 
-public class PlaylistMusicas {
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifs.designpatterns.iterator.Agregador;
+import br.edu.ifs.designpatterns.iterator.Iterador;
+
+
+public class PlaylistMusicas implements Agregador<Musica>{
+	
+	private List<Musica> musicas = new ArrayList<>();
+	private IteradorMusicas iteradorMusicas;
+
+	@Override
+	public Iterador<Musica> criarIterador() {
+		// TODO Auto-generated method stub
+		
+		return this.setIteradorMusicas(new IteradorMusicas(this.musicas));
+	}
+
+	public void adicionar(Musica musica) {
+		// TODO Auto-generated method stub
+		this.musicas.add(musica);
+	}
+
+	public IteradorMusicas getIteradorMusicas() {
+		return iteradorMusicas;
+	}
+
+	private IteradorMusicas setIteradorMusicas(IteradorMusicas iteradorMusicas) {
+		this.iteradorMusicas = iteradorMusicas;
+		return iteradorMusicas;
+	}
 
 }
