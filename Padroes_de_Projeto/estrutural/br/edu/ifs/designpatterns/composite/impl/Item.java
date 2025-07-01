cdde8c12cf8b4f09c3a4f774107abc85fc961af8
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 21:58:27 -0300

    feat(Leaf): implementar classe Item com atributo valor e lógica de calcularPreco

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/composite/impl/Item.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/composite/impl/Item.java
@@ -1,5 +1,23 @@
 package br.edu.ifs.designpatterns.composite.impl;
 
-public class Item {
+import br.edu.ifs.designpatterns.composite.Componente;
+
+public class Item extends Componente {
+
+	private double valor;
+	
+	public Item(String nome, double valor) {
+		super(nome);
+		// TODO Auto-generated method stub
+		this.valor = valor;
+	}
+
+	@Override
+	public double calcularPreco() {
+		// TODO Auto-generated method stub
+		return this.valor;
+	}
+
+	
 
 }
