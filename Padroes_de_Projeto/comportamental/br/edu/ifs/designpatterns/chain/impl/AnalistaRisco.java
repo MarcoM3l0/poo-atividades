52fe52ca1a6e2843c884fba0c3d90416a275dfff
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 23:49:48 -0300

    feat(Concrete Handlers): implementar AnalistaRisco com validação de score de crédito

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/chain/impl/AnalistaRisco.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/chain/impl/AnalistaRisco.java
@@ -1,5 +1,18 @@
 package br.edu.ifs.designpatterns.chain.impl;
 
-public class AnalistaRisco {
+import br.edu.ifs.designpatterns.chain.Analisador;
+
+public class AnalistaRisco extends Analisador {
+
+	@Override
+	public Emprestimo processarRequisição(RequisicaoEmprestimo requisicao) {
+		// TODO Auto-generated method stub
+		
+		if(requisicao.getScoreCredito() < 700) {
+			throw new IllegalStateException("Analista de risco: Reprovado! Score de crédito baixo.");
+		}
+		
+		return sucessor.processarRequisição(requisicao);
+	}
 
 }
