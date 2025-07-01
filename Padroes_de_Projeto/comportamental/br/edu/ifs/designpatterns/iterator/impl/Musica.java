0d70c45f51848f6773243c2450740f23383f2555
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 19:11:53 -0300

    feat(Domain): adicionar classe Musica como entidade base para playlists do padrão Iterator

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/iterator/impl/Musica.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/iterator/impl/Musica.java
@@ -1,5 +1,24 @@
 package br.edu.ifs.designpatterns.iterator.impl;
 
 public class Musica {
+	
+	private String titulo;
+	private String artista;
+	
+	public Musica(String titulo, String artista) {
+		super();
+		this.titulo = titulo;
+		this.artista = artista;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getArtista() {
+		return artista;
+	}
+	
+	
 
 }
