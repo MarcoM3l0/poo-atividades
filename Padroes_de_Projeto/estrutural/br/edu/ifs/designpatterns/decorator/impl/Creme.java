5219060f918cb1572db811286d24a6e2e843d30a
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 22:55:02 -0300

    feat(Concrete Decorator): implementar classe Creme como decorador de Component

--- Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/decorator/impl/Creme.java
+++ Padroes_de_Projeto/estrutural/br/edu/ifs/designpatterns/decorator/impl/Creme.java
@@ -1,5 +1,24 @@
 package br.edu.ifs.designpatterns.decorator.impl;
 
-public class Creme {
+import br.edu.ifs.designpatterns.decorator.Component;
+
+public class Creme extends Decorator {
+
+	public Creme(Component component) {
+		super(component);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double custo() {
+		// TODO Auto-generated method stub
+		return super.component.custo() + 1;
+	}
+
+	@Override
+	public String getDescricao() {
+		// TODO Auto-generated method stub
+		return super.component.getDescricao() + ", com creme";
+	}
 
 }
