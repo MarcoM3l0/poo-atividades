80040407fba15528f9d43cf2f75bc8cd508fe8f2
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 23:32:43 -0300

    feat(Request): adicionar classe RequisicaoEmprestimo como objeto de entrada da cadeia de análise

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/chain/impl/RequisicaoEmprestimo.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/chain/impl/RequisicaoEmprestimo.java
@@ -1,5 +1,28 @@
 package br.edu.ifs.designpatterns.chain.impl;
 
 public class RequisicaoEmprestimo {
+	
+	private double valor;
+	private double renda;
+	private int scoreCredito;
+	
+	public RequisicaoEmprestimo(double valor, double renda, int scoreCredito) {
+		super();
+		this.valor = valor;
+		this.renda = renda;
+		this.scoreCredito = scoreCredito;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public double getRenda() {
+		return renda;
+	}
+
+	public int getScoreCredito() {
+		return scoreCredito;
+	} 
 
 }
