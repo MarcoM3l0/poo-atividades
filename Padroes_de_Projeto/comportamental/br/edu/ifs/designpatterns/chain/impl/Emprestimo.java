a3bc7ebefd2cb336ce4c826979ad90c58a64cfbe
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-06-30 23:24:13 -0300

    feat(Response): implementar classe Emprestimo como resposta do processamento da requisição

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/chain/impl/Emprestimo.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/chain/impl/Emprestimo.java
@@ -1,5 +1,16 @@
 package br.edu.ifs.designpatterns.chain.impl;
 
 public class Emprestimo {
+	
+	private double valor;
+
+	public Emprestimo(double valor) {
+		super();
+		this.valor = valor;
+	}
+
+	public double getValor() {
+		return valor;
+	}
 
 }
