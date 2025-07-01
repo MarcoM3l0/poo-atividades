924750fb603b058f575d87fe75f2db049952d2ae
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 20:38:25 -0300

    feat(Concrete Collection): implementar PlaylistArtistas com agrupamento por artista

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/iterator/impl/PlaylistArtistas.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/iterator/impl/PlaylistArtistas.java
@@ -1,5 +1,40 @@
 package br.edu.ifs.designpatterns.iterator.impl;
 
-public class PlaylistArtistas {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.edu.ifs.designpatterns.iterator.Agregador;
+import br.edu.ifs.designpatterns.iterator.Iterador;
+
+public class PlaylistArtistas implements Agregador<Musica>{
+	
+	private Map<String, List<Musica>> musicas = new HashMap<>();
+	private IteradorArtistas iteradorArtistas;
+
+	public void adicionar(Musica musica) {
+		// TODO Auto-generated method stub
+		musicas
+			.computeIfAbsent(musica.getArtista(), chave -> new ArrayList<>())
+			.add(musica);
+	}
+
+	public Iterador<Musica> criarIterador() {
+		// TODO Auto-generated method stub
+		
+		List<Musica> todasMusicas = new ArrayList<>();
+		
+		for(List<Musica> musicasPorArtista : musicas.values()) {
+			todasMusicas.addAll(musicasPorArtista);
+		}
+		
+		return setIteradorArtistas(new IteradorArtistas(todasMusicas));
+	}
+
+	private IteradorArtistas setIteradorArtistas(IteradorArtistas iteradorArtistas) {
+		this.iteradorArtistas = iteradorArtistas;
+		return iteradorArtistas;
+	}
 
 }
