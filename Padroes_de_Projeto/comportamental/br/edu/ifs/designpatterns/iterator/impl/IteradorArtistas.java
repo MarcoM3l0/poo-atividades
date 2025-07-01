b34cc107f7dde9cd17cee90f1dd5d24f4d64b106
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 20:38:59 -0300

    feat(Concrete Iterator): criar IteradorArtistas para navegar músicas por artista

---
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/iterator/impl/IteradorArtistas.java
@@ -0,0 +1,46 @@
+package br.edu.ifs.designpatterns.iterator.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifs.designpatterns.iterator.Iterador;
+
+public class IteradorArtistas implements Iterador<Musica> {
+
+	private List<Musica> playlist = new ArrayList<>();
+
+	public IteradorArtistas(List<Musica> playlist) {
+		super();
+		this.setPlaylist(playlist);
+	}
+
+	@Override
+	public Musica proximo() {
+		// TODO Auto-generated method stub
+		if(playlist.size() == 0)
+			return null;
+		
+		Musica proxima = playlist.get(0);
+		playlist.remove(proxima);
+		
+		return proxima;
+	}
+
+	@Override
+	public boolean temProximo() {
+		// TODO Auto-generated method stub
+		if(playlist.size() == 0)
+			return false;
+		
+		return true;
+	}
+
+	public List<Musica> getPlaylist() {
+		return playlist;
+	}
+
+	private void setPlaylist(List<Musica> playlist) {
+		this.playlist = playlist;
+	}
+
+}
