aa1a0b1861f89dd7461d2d957485e03b03a3b64c
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 00:23:06 -0300

    feat(Receiver): adicionar classes Televisao e Luz como receptores do padrão Command com lógica de ligar/desligar

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/Luz.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/Luz.java
@@ -1,5 +1,27 @@
 package br.edu.ifs.designpatterns.command;
 
+
 public class Luz {
+	
+	private boolean ligada;
+	
+	public Luz(String nome) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void ligar() {
+		if(!ligada)
+			this.ligada = !this.ligada;
+	}
+
+	public void desligar() {
+		if(ligada)
+			this.ligada = !this.ligada;
+	}
+
+	public boolean estaLigada() {
+		// TODO Auto-generated method stub
+		return this.ligada;
+	}
 
 }
--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/Televisao.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/command/Televisao.java
@@ -1,5 +1,26 @@
 package br.edu.ifs.designpatterns.command;
 
-public class Televisao {
 
+public class Televisao {
+	
+	private boolean ligada;
+	
+	public Televisao(String nome) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void ligar() {
+		if(!ligada)
+			this.ligada = !this.ligada;
+	}
+
+	public void desligar() {
+		if(ligada)
+			this.ligada = !this.ligada;
+	}
+
+	public boolean estaLigada() {
+		// TODO Auto-generated method stub
+		return this.ligada;
+	}
 }
