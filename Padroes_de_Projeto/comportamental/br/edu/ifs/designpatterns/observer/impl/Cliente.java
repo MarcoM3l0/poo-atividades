55376cd77936052ce06a65e3a81460d73057d56f
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 02:01:22 -0300

    feat(Concrete Observer): adicionar Cliente como observador reativo que realiza compras ao ser notificado por Produto

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/observer/impl/Cliente.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/observer/impl/Cliente.java
@@ -1,5 +1,39 @@
 package br.edu.ifs.designpatterns.observer.impl;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifs.designpatterns.observer.Observador;
+import br.edu.ifs.designpatterns.observer.Sujeito;
+
+public class Cliente implements Observador {
+	
+	private String nome;
+	private List<String> pedidos = new ArrayList<>();
+
+	public Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	@Override
+	public void atualizar(Sujeito sujeito) {
+		// TODO Auto-generated method stub
+		if(sujeito instanceof Produto) {
+			Produto produto = (Produto) sujeito;
+			comprar(produto);
+		}
+	}
+
+	public void comprar(Produto produto) {
+		// TODO Auto-generated method stub
+		
+		produto.remover(1);
+		pedidos.add(produto.getNome());
+	}
+
+	public List<String> getPedidos() {
+		return pedidos;
+	}
 
 }
