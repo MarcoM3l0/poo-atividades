2a2c27f641d60adbe896393fe940d8e827f542f0
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 20:53:51 -0300

    feat(Concrete State): adicionar MarioMorto como estado terminal no padrão State, com exceções para ações inválidas

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/state/impl/MarioMorto.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/state/impl/MarioMorto.java
@@ -8,37 +8,40 @@ public class MarioMorto implements EstadoMario {
 	@Override
 	public void pegarCogumelo(Mario mario) {
 		// TODO Auto-generated method stub
-		
+		throw new IllegalStateException("Mario está sem vida!");
 	}
 
 	@Override
 	public void pegarFlor(Mario mario) {
 		// TODO Auto-generated method stub
+		throw new IllegalStateException("Mario está sem vida!");
 		
 	}
 
 	@Override
 	public void pegarPena(Mario mario) {
 		// TODO Auto-generated method stub
+		throw new IllegalStateException("Mario está sem vida!");
 		
 	}
 
 	@Override
 	public void sofrerDano(Mario mario) {
 		// TODO Auto-generated method stub
+		throw new IllegalStateException("Mario está sem vida!");
 		
 	}
 
 	@Override
 	public String obterAtque() {
 		// TODO Auto-generated method stub
-		return null;
+		throw new IllegalStateException("Mario está sem vida!");
 	}
 
 	@Override
 	public String obterEstado() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Mario morto";
 	}
 
 }
