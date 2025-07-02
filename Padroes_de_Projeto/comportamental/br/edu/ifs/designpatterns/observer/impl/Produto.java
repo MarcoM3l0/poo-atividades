f5527f6aa7772d92752b97a0fddc41689c9cd48d
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 01:42:35 -0300

    feat(Concrete Subject): adicionar Produto como sujeito concreto que dispara notificações ao adicionar unidades

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/observer/impl/Produto.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/observer/impl/Produto.java
@@ -1,5 +1,47 @@
 package br.edu.ifs.designpatterns.observer.impl;
 
-public class Produto {
+import br.edu.ifs.designpatterns.observer.Sujeito;
+
+public class Produto extends Sujeito {
+	
+	private String nome;
+	private int quantidade;
+	
+	public Produto(String nome) {
+		super();
+		this.nome = nome;
+	}
+	
+	public void adicionar(int quantidade) {
+		if(quantidade <= 0)
+			return;
+		
+		this.quantidade = quantidade;
+		
+		if(estaDisponivel())
+			super.notificar();
+	}
+	
+	public void remover(int quantidade) {
+		if(quantidade <= 0)
+			return;
+		
+		if(this.quantidade < quantidade)
+			throw new IllegalStateException("Quantidade insuficiente");
+		
+		this.quantidade -= quantidade;
+	}
+	
+	public boolean estaDisponivel() {
+		return this.quantidade > 0;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
 
 }
