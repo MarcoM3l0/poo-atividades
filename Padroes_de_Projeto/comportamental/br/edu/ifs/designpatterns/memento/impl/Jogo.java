454b8312271f147735a7cba1bea67a9a86821b1f
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 00:37:22 -0300

    feat(Originator): adicionar Jogo como Originador no padrão Memento com lógica de dano e persistência de estado

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/memento/impl/Jogo.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/memento/impl/Jogo.java
@@ -1,5 +1,36 @@
 package br.edu.ifs.designpatterns.memento.impl;
 
 public class Jogo {
+	
+	private int saude = 100;
+	private int vidas = 3;
+	
+	public Memento salvar() {
+		return new Memento(this.saude, this.vidas);
+	}
+
+	public void causarDano(int dano) {
+		// TODO Auto-generated method stub
+		
+		this.saude -= dano;
+		
+		if(this.saude <= 0) {
+			if(this.vidas <= 1)
+				throw new IllegalStateException("Fim de Jogo");
+			
+			this.saude = 100;
+			this.vidas--;
+		}
+	}
+	
+	public void restaurar(Memento momento) {
+		this.saude = momento.getSaude();
+		this.vidas = momento.getVida();
+	}
+
+	@Override
+	public String toString() {
+		return "{saude: " + saude + ", vidas: " + vidas + "}";
+	}
 
 }
