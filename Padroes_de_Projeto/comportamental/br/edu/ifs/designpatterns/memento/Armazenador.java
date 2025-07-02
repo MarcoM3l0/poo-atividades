177a6d16c776ea8213cc982d4e13324a240f53cf
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 00:58:33 -0300

    feat(Caretaker): adicionar Armazenador como guardião dos estados salvos do Jogo no padrão Memento

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/memento/Armazenador.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/memento/Armazenador.java
@@ -1,5 +1,35 @@
 package br.edu.ifs.designpatterns.memento;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifs.designpatterns.memento.impl.Memento;
+
 public class Armazenador {
+	
+	private List<Memento> mementos = new ArrayList<>();
+	private List<String> mementosHistorico = new ArrayList<>();
+
+	public void armazenar(Memento salvar) {
+		// TODO Auto-generated method stub
+		mementos.add(salvar);
+	}
+
+	public Memento recuperar() {
+		// TODO Auto-generated method stub
+		
+		if(mementos.size() == 0)
+			throw new IllegalStateException("Histórico vazio");
+		
+		Memento recuperar = mementos.get(mementos.size() - 1);
+		mementos.remove(recuperar);
+		mementosHistorico.add(recuperar.toString());
+		
+		return recuperar;
+	}
+	
+	public String recuperarHistorico() {
+		return mementosHistorico.toString();
+	}
 
 }
