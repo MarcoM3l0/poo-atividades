1c517fd7fa17983758521235818cfdabb0c1f74f
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 23:19:08 -0300

    feat(Concrete Colleague): adicionar Aeronave como participante concreto que se comunica com o ControladorTrafego

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/mediator/impl/Aeronave.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/mediator/impl/Aeronave.java
@@ -1,5 +1,21 @@
 package br.edu.ifs.designpatterns.mediator.impl;
 
-public class Aeronave {
+import br.edu.ifs.designpatterns.mediator.Colaborador;
+import br.edu.ifs.designpatterns.mediator.Mediador;
+
+public class Aeronave extends Colaborador{
+
+	public Aeronave(Mediador mediador, String id) {
+		super(mediador, id);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String solicitarPouso() {
+		return super.mediador.notificar(this, "pouso");
+	}
+	
+	public String solicitarDecolagem() {
+		return super.mediador.notificar(this, "decolagem");
+	}
 
 }
