ab55c7ac777c53fded8321512312f0cbedf484b3
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 22:13:03 -0300

    feat(Colleague): adicionar classe abstrata Colaborador com referência ao Mediador

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/mediator/Colaborador.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/mediator/Colaborador.java
@@ -3,5 +3,15 @@ package br.edu.ifs.designpatterns.mediator;
 public abstract class Colaborador {
 	
 	protected Mediador mediador;
+	protected String id;
+	
+	public Colaborador(Mediador mediador, String id) {
+		this.mediador = mediador;
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
 
 }
