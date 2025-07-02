2cb4bb94bda59df7d8b4b2b88f5654224e84f154
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 20:31:30 -0300

    feat(Context): adicionar Mario como classe de controle de estado com transições e lógica de ataque dinâmica no padrão State

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/state/Mario.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/state/Mario.java
@@ -1,5 +1,51 @@
 package br.edu.ifs.designpatterns.state;
 
-public class Mario {
+import br.edu.ifs.designpatterns.state.impl.*;
 
+public class Mario {
+	
+	private EstadoMario estado;
+	
+	public Mario() {
+		this.estado = new MarioPequeno();
+	}
+
+	public String atacar() {
+		
+		return this.estado.obterAtque();
+	}
+	
+	public void pegarCogumelo() {
+		
+		if((this.estado instanceof MarioFogo) || (this.estado instanceof MarioVoador))
+			return;
+		
+		if(!(this.estado instanceof MarioSuper))
+			this.estado.pegarCogumelo(this);
+	}
+	
+	public void pegarFlor() {
+		if(!(this.estado instanceof MarioFogo))
+			this.estado.pegarFlor(this);
+	}
+	
+	public void pegarPena() {
+		if(!(this.estado instanceof MarioVoador))
+			this.estado.pegarPena(this);
+	}
+	
+	public void sofreDano() {
+		this.estado.sofrerDano(this);
+	}
+
+	public String obterEstado() {
+		// TODO Auto-generated method stub
+		return this.estado.obterEstado();
+	}
+
+	public void setEstado(EstadoMario estado) {
+		this.estado = estado;
+	}
+
+	
 }
--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/state/impl/MarioFogo.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/state/impl/MarioFogo.java
@@ -1,7 +1,44 @@
 package br.edu.ifs.designpatterns.state.impl;
 
 import br.edu.ifs.designpatterns.state.EstadoMario;
+import br.edu.ifs.designpatterns.state.Mario;
 
 public class MarioFogo implements EstadoMario {
+
+	@Override
+	public void pegarCogumelo(Mario mario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pegarFlor(Mario mario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pegarPena(Mario mario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sofrerDano(Mario mario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String obterAtque() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String obterEstado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
