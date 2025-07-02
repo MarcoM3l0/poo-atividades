8371775787ef666f4b26786299e49a292f44df8a
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 00:29:25 -0300

    feat(Memento): adicionar classe Memento com encapsulamento do estado do Originador no padrão Memento

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/memento/impl/Memento.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/memento/impl/Memento.java
@@ -3,19 +3,19 @@ package br.edu.ifs.designpatterns.memento.impl;
 public class Memento {
 	
 	private int saude;
-	private int vida;
+	private int vidas;
 	
 	public Memento(int saude, int vida) {
 		super();
 		this.saude = saude;
-		this.vida = vida;
+		this.vidas = vida;
 	}
 	
 	public int getSaude() {
 		return saude;
 	}
 	public int getVida() {
-		return vida;
+		return vidas;
 	}
 
 }
