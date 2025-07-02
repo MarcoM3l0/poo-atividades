7eea4df59e82c24138f69b1fb9828ea8e9d1388e
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-01 23:29:23 -0300

    feat(Concrete Colleague): adicionar Metereologia como participante do padrão Mediator com lógica de notificação ao controlador

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/mediator/impl/Metereologia.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/mediator/impl/Metereologia.java
@@ -1,5 +1,35 @@
 package br.edu.ifs.designpatterns.mediator.impl;
 
-public class Metereologia {
+import br.edu.ifs.designpatterns.mediator.Colaborador;
+import br.edu.ifs.designpatterns.mediator.Mediador;
+
+public class Metereologia extends Colaborador {
+
+	private boolean condicoesFavoraveis;
+	
+	public Metereologia(Mediador mediador, String id) {
+		super(mediador, id);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public String informarCondicoes() {
+		
+		if(isCondicoesFavoraveis())
+			return super.mediador.notificar(this, "condicoesFavoraveis");
+		
+		return "Condições metereológicas desfavoraveis.";
+		
+	}
+
+
+	public boolean isCondicoesFavoraveis() {
+		return condicoesFavoraveis;
+	}
+
+
+	public void setCondicoesFavoraveis(boolean condicoesFavoraveis) {
+		this.condicoesFavoraveis = condicoesFavoraveis;
+	}
 
 }
