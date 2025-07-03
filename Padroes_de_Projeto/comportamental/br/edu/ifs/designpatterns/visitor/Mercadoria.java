47c05a29778a65341d6b684ad9002fb82af33c42
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 22:56:15 -0300

    feat(Element): implementar classe abstrata Mercadoria com suporte a visitante no padrão Visitor

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/Mercadoria.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/Mercadoria.java
@@ -1,5 +1,18 @@
 package br.edu.ifs.designpatterns.visitor;
 
 public abstract class Mercadoria {
+	
+	private double custo;
+
+	public Mercadoria(double custo) {
+		super();
+		this.custo = custo;
+	}
+
+	public double getCusto() {
+		return custo;
+	}
+
+	public abstract double aceitar(Taxador taxador);
 
 }
