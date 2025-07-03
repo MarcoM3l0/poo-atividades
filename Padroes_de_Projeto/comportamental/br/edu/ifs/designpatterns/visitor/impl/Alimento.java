2c3ca253478aa2a23fe166c2eccce238cb4f030d
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 23:00:28 -0300

    feat(Concrete Element): adicionar mercadorias concretas que aceitam Taxador via Visitor para cálculo de impostos customizados

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/impl/Alimento.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/impl/Alimento.java
@@ -1,7 +1,19 @@
 package br.edu.ifs.designpatterns.visitor.impl;
 
 import br.edu.ifs.designpatterns.visitor.Mercadoria;
+import br.edu.ifs.designpatterns.visitor.Taxador;
 
 public class Alimento extends Mercadoria {
+
+	public Alimento(double custo) {
+		super(custo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double aceitar(Taxador taxador) {
+		// TODO Auto-generated method stub
+		return taxador.visitar(this);
+	}
 
 }
--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/impl/Cerveja.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/impl/Cerveja.java
@@ -1,7 +1,19 @@
 package br.edu.ifs.designpatterns.visitor.impl;
 
 import br.edu.ifs.designpatterns.visitor.Mercadoria;
+import br.edu.ifs.designpatterns.visitor.Taxador;
 
 public class Cerveja extends Mercadoria {
+
+	public Cerveja(double custo) {
+		super(custo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double aceitar(Taxador taxador) {
+		// TODO Auto-generated method stubv
+		return taxador.visitar(this);
+	}
 
 }
