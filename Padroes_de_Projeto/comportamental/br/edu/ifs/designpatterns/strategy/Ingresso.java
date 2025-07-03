d7ff1c765e7940a32e8c921393b5f16aefe33ca9
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 21:38:41 -0300

    feat(Context): adicionar Ingresso como contexto no padrão Strategy com delegação de cálculo para estratégias de desconto

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/strategy/Ingresso.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/strategy/Ingresso.java
@@ -1,5 +1,18 @@
 package br.edu.ifs.designpatterns.strategy;
 
 public class Ingresso {
+	
+	private double valor;
+	private EstrategiaDesconto estrategia;
+	
+	public Ingresso(double valor, EstrategiaDesconto estrategia) {
+		super();
+		this.valor = valor;
+		this.estrategia = estrategia;
+	}
+
+	public double calcularValor() {
+		return this.valor * this.estrategia.calcularDesconto();
+	}
 
 }
