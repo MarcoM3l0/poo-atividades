ba82ddf83e95f480cfd3b50e2cbd46bfd2b518eb
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 23:04:23 -0300

    feat(Concrete Visitor): implementar TaxadorFederal com regras de imposto específicas por tipo de mercadoria

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/impl/TaxadorFederal.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/impl/TaxadorFederal.java
@@ -4,4 +4,28 @@ import br.edu.ifs.designpatterns.visitor.Taxador;
 
 public class TaxadorFederal implements Taxador {
 
+	@Override
+	public double visitar(Alimento alimento) {
+		// TODO Auto-generated method stub
+		return alimento.getCusto() * 1.11;
+	}
+
+	@Override
+	public double visitar(Cerveja cerveja) {
+		// TODO Auto-generated method stub
+		return cerveja.getCusto() * 2.5;
+	}
+
+	@Override
+	public double visitar(Cigarro cigarro) {
+		// TODO Auto-generated method stub
+		return cigarro.getCusto() * 1.16;
+	}
+
+	@Override
+	public double visitar(Vinho vinho) {
+		// TODO Auto-generated method stub
+		return vinho.getCusto() * 1.6;
+	}
+
 }
