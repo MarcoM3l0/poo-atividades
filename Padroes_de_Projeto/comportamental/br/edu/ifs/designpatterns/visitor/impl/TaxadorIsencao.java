f47c2a918e8ad49df3896eaebc78536afb1c0e38
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 23:09:41 -0300

    feat(Concrete Visitor): implementar TaxadorIsencao com isenção para alimentos e alíquotas reduzidas para demais produtos

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/impl/TaxadorIsencao.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/impl/TaxadorIsencao.java
@@ -4,4 +4,28 @@ import br.edu.ifs.designpatterns.visitor.Taxador;
 
 public class TaxadorIsencao implements Taxador {
 
+	@Override
+	public double visitar(Alimento alimento) {
+		// TODO Auto-generated method stub
+		return alimento.getCusto();
+	}
+
+	@Override
+	public double visitar(Cerveja cerveja) {
+		// TODO Auto-generated method stub
+		return cerveja.getCusto() * 2.5;
+	}
+
+	@Override
+	public double visitar(Cigarro cigarro) {
+		// TODO Auto-generated method stub
+		return cigarro.getCusto() * 1.15;
+	}
+
+	@Override
+	public double visitar(Vinho vinho) {
+		// TODO Auto-generated method stub
+		return vinho.getCusto() * 1.2;
+	}
+
 }
