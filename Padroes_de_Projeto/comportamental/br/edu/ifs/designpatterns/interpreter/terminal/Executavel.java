3d3eed4d914b6b0b943fc51c091c6499cccaf7a9
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-03 00:18:18 -0300

    feat(Terminal Expression): adicionar Executavel como expressão terminal baseada no padrão Command que executa ações durante a interpretação

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/terminal/Executavel.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/terminal/Executavel.java
@@ -3,19 +3,25 @@ package br.edu.ifs.designpatterns.interpreter.terminal;
 import br.edu.ifs.designpatterns.command.Comando;
 import br.edu.ifs.designpatterns.interpreter.ExpressaoTerminal;
 
-public class Executavel extends ExpressaoTerminal<Object> {
-	
-	private Comando comando;
+public class Executavel extends ExpressaoTerminal<Comando> {
 
-	public Executavel(Object obj) {
+	private Comando comando;
+	
+	public Executavel(Comando contexto) {
+		super(contexto);
 		// TODO Auto-generated constructor stub
-		this.comando = (Comando) obj;
+		this.comando = contexto;
 	}
 
 	@Override
 	public boolean interpretar() {
 		// TODO Auto-generated method stub
-		return comando.executar();
+		try {
+			this.comando.executar();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 
