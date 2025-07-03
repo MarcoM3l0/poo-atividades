121ec13c0f8331fe500490f661f3b5ad3c01ec3b
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 22:26:19 -0300

    feat(Concrete Class): adicionar Cafe como implementação concreta do Template Method com ingrediente específico no preparo

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/template/impl/Cafe.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/template/impl/Cafe.java
@@ -1,5 +1,20 @@
 package br.edu.ifs.designpatterns.template.impl;
 
-public class Cafe {
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifs.designpatterns.template.Bebida;
+
+public class Cafe extends Bebida {
+
+	@Override
+	protected List<String> prepararIngredientes() {
+		// TODO Auto-generated method stub
+List<String> ingrediente = new ArrayList<>();
+		
+		ingrediente.add("Adicionar uma colher de café");
+		
+		return ingrediente;
+	}
 
 }
