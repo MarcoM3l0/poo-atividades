f2cf4b3b99b0806cde69de559dde419b1c272915
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 22:28:20 -0300

    feat(Concrete Class): adicionar Cappuccino como implementação concreta do Template Method com ingrediente específico no preparo

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/template/impl/Cappuccino.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/template/impl/Cappuccino.java
@@ -1,5 +1,22 @@
 package br.edu.ifs.designpatterns.template.impl;
 
-public class Cappuccino {
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifs.designpatterns.template.Bebida;
+
+public class Cappuccino extends Bebida {
+
+	@Override
+	protected List<String> prepararIngredientes() {
+		// TODO Auto-generated method stub
+		List<String> ingredientes = new ArrayList<>();
+		
+		ingredientes.add("Adicionar uma colher de café");
+		ingredientes.add("Adicionar uma colher de leite em pó");
+		ingredientes.add("Adicionar uma colher de chocolate em pó");
+		
+		return ingredientes;
+	}
 
 }
