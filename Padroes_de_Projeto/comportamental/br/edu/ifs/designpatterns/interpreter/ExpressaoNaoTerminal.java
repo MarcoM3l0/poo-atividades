c6197ced9629d3f71d67604494ea1f86ae496cc9
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-03 20:57:12 -0300

    feat(Nonterminal Expression): implementar ExpressaoNaoTerminal com suporte a composição de múltiplas subexpressões interpretáveis

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/ExpressaoNaoTerminal.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/ExpressaoNaoTerminal.java
@@ -1,5 +1,19 @@
 package br.edu.ifs.designpatterns.interpreter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class ExpressaoNaoTerminal extends Expressao {
+	
+	private List<Expressao> expressoes = new ArrayList<>();
+
+	public ExpressaoNaoTerminal(List<Expressao> expressoes) {
+		super();
+		this.expressoes = expressoes;
+	}
+
+	public List<Expressao> getExpressoes() {
+		return expressoes;
+	}
 
 }
