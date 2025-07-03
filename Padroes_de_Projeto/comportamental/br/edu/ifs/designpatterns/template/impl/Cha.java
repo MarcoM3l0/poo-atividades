19487c9759c417c84aa0b0da1d372cca094f9825
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 22:20:07 -0300

    feat(Template Method): adicionar classe abstrata Bebida com algoritmo base de preparo e gancho para ingredientes personalizados

---
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/template/impl/Cha.java
@@ -0,0 +1,15 @@
+package br.edu.ifs.designpatterns.template.impl;
+
+import java.util.List;
+
+import br.edu.ifs.designpatterns.template.Bebida;
+
+public class Cha extends Bebida {
+
+	@Override
+	protected List<String> prepararIngredientes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
