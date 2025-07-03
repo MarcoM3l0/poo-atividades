554bca6ec5b7906db02f4bc5b42ed61a74f5b5ca
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 22:43:38 -0300

    feat(Visitor): definir interface Taxador com métodos especializados para tipos de produtos visitáveis

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/Taxador.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/visitor/Taxador.java
@@ -1,5 +1,15 @@
 package br.edu.ifs.designpatterns.visitor;
 
+import br.edu.ifs.designpatterns.visitor.impl.Alimento;
+import br.edu.ifs.designpatterns.visitor.impl.Cerveja;
+import br.edu.ifs.designpatterns.visitor.impl.Cigarro;
+import br.edu.ifs.designpatterns.visitor.impl.Vinho;
+
 public interface Taxador {
+
+	public double visitar(Alimento alimento);
+	public double visitar(Cerveja cerveja);
+	public double visitar(Cigarro cigarro);
+	public double visitar(Vinho vinho);
 
 }
