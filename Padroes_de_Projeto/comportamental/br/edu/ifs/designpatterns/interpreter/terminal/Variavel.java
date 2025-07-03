7691359801dae3f46b7b17369166d9f1bf215d72
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-03 20:49:38 -0300

    feat(Terminal Expression): adicionar Variavel como terminal genérico no padrão Interpreter com nome identificador e verificação de existência

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/terminal/Variavel.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/terminal/Variavel.java
@@ -2,6 +2,31 @@ package br.edu.ifs.designpatterns.interpreter.terminal;
 
 import br.edu.ifs.designpatterns.interpreter.ExpressaoTerminal;
 
-public class Variavel extends ExpressaoTerminal {
+public class Variavel<G extends Comparable<? super G>> extends ExpressaoTerminal<G> {
+	
+	private String nome;
 
+	public Variavel( String nome, G contexto) {
+		super(contexto);
+		this.nome = nome;
+	}
+
+	public Variavel(G contexto) {
+		super(contexto);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean interpretar() {
+		// TODO Auto-generated method stub
+		return super.getContexto() != null;
+	}
+	
+	 public G getValor() {
+        return super.getContexto();
+    }
+
+	public String getNome() {
+		return nome;
+	}
 }
