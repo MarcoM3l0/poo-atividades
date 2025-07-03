590f334247cc640ce6b6887eb293005b33813a06
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-02 21:15:03 -0300

    feat(Concrete State): adicionar MarioVoador como estado avançado no padrão State com manutenção de poder e evolução para MarioFogo e dowgrede por dano para MarioSuper

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/state/impl/MarioVoador.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/state/impl/MarioVoador.java
@@ -8,37 +8,37 @@ public class MarioVoador implements EstadoMario {
 	@Override
 	public void pegarCogumelo(Mario mario) {
 		// TODO Auto-generated method stub
-		
+		mario.setEstado(this);
 	}
 
 	@Override
 	public void pegarFlor(Mario mario) {
 		// TODO Auto-generated method stub
-		
+		mario.setEstado(new MarioFogo());
 	}
 
 	@Override
 	public void pegarPena(Mario mario) {
 		// TODO Auto-generated method stub
-		
+		mario.setEstado(this);
 	}
 
 	@Override
 	public void sofrerDano(Mario mario) {
 		// TODO Auto-generated method stub
-		
+		mario.setEstado(new MarioSuper());
 	}
 
 	@Override
 	public String obterAtque() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Capa giratória";
 	}
 
 	@Override
 	public String obterEstado() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Mario voador";
 	}
 
 }
