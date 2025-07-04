98444664b32cd6a4c454dafe2a5effcb6df42412
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-03 22:12:59 -0300

    feat(Interpreter): implementar ExpressaoCondicional com execução condicional baseada em expressão relacional

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/conditional/ExpressaoCondicional.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/conditional/ExpressaoCondicional.java
@@ -1,7 +1,28 @@
 package br.edu.ifs.designpatterns.interpreter.conditional;
 
+import br.edu.ifs.designpatterns.interpreter.Expressao;
 import br.edu.ifs.designpatterns.interpreter.ExpressaoNaoTerminal;
+import br.edu.ifs.designpatterns.interpreter.relational.ExpressaoRelacional;
 
 public class ExpressaoCondicional extends ExpressaoNaoTerminal {
+	
+	private Expressao expressao;
+	private ExpressaoRelacional<?> relacao;
+	
+	public ExpressaoCondicional(ExpressaoRelacional<?> relacao, Expressao expressao) {
+		super();
+		this.expressao = expressao;
+		this.relacao = relacao;
+	}
+
+	@Override
+	public boolean interpretar() {
+		// TODO Auto-generated method stub
+		
+		if(this.relacao.interpretar())
+			return this.expressao.interpretar();
+		
+		return false;
+	}
 
 }
