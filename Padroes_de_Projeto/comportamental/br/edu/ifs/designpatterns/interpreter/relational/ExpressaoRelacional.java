ca76e98237966f229740718ea8cfe65bef063bcb
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-03 21:34:37 -0300

    feat(Nonterminal Expression): implementar ExpressaoRelacional com suporte genérico a comparações binárias

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/relational/ExpressaoRelacional.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/relational/ExpressaoRelacional.java
@@ -1,7 +1,19 @@
 package br.edu.ifs.designpatterns.interpreter.relational;
 
 import br.edu.ifs.designpatterns.interpreter.ExpressaoNaoTerminal;
+import br.edu.ifs.designpatterns.interpreter.terminal.Variavel;
 
-public abstract class ExpressaoRelacional extends ExpressaoNaoTerminal {
+public abstract class ExpressaoRelacional<T extends Comparable<? super T>> extends ExpressaoNaoTerminal {
+	
+	protected String nome;
+	protected Variavel<T> termo1;
+	protected Variavel<T> termo2;
+	
+	public ExpressaoRelacional(String nome, Variavel<T> termo1, Variavel<T> termo2) {
+		super();
+		this.nome = nome;
+		this.termo1 = termo1;
+		this.termo2 = termo2;
+	}
 
 }
