1bbec78052f4ac254c3b0a83fa086b68421913cd
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2025-07-03 21:45:27 -0300

    feat(Relational Expression): implementar ExpressaoMaior e ExpressaoIgual com base genérica comparável

--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/relational/ExpressaoIgual.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/relational/ExpressaoIgual.java
@@ -1,5 +1,18 @@
 package br.edu.ifs.designpatterns.interpreter.relational;
 
-public class ExpressaoIgual extends ExpressaoRelacional {
+import br.edu.ifs.designpatterns.interpreter.terminal.Variavel;
+
+public class ExpressaoIgual<T extends Comparable<? super T>> extends ExpressaoRelacional<T> {
+
+	public ExpressaoIgual(Variavel<T> termo1, Variavel<T> termo2) {
+		super(termo1, termo2);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean interpretar() {
+		// TODO Auto-generated method stub
+		return super.termo1.getValor().compareTo(super.termo2.getValor()) == 0;
+	}
 
 }
--- Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/relational/ExpressaoMaior.java
+++ Padroes_de_Projeto/comportamental/br/edu/ifs/designpatterns/interpreter/relational/ExpressaoMaior.java
@@ -4,15 +4,15 @@ import br.edu.ifs.designpatterns.interpreter.terminal.Variavel;
 
 public class ExpressaoMaior<T extends Comparable<? super T>> extends ExpressaoRelacional<T> {
 
-	public ExpressaoMaior(String nome, Variavel<T> termo1, Variavel<T> termo2) {
-		super(nome, termo1, termo2);
+	public ExpressaoMaior(Variavel<T> termo1, Variavel<T> termo2) {
+		super(termo1, termo2);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean interpretar() {
 		// TODO Auto-generated method stub
-		return false;
+		return super.termo1.getValor().compareTo(super.termo2.getValor()) > 0;
 	}
 
 }
